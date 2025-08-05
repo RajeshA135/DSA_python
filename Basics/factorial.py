64e018cc1efcc8c754a1f4826b917930f96a36a2
Rajesh Allam <rajeshallam9966@gmail.com>
2025-08-05 19:30:01 +0530

    Added factiorial of a number

---
+++ Basics/factorial.py
@@ -0,0 +1,10 @@
+#Find factorial of a number
+def factorial(n):
+    if n<0: 
+        return "Factorial does not exist for negative numbers"
+    elif n==0 or n==1:
+        return 1
+    else:
+        return n * factorial(n-1)
+n=int(input("Enter a number:"))
+print(factorial(n))
