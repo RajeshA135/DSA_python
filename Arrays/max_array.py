a66b444e077b20800b44fd2598af98b77a5abe4d
Rajesh Allam <rajeshallam9966@gmail.com>
2025-08-12 12:36:44 +0530

    Added max_array

---
+++ Arrays/max_array.py
@@ -0,0 +1,9 @@
+#finding minimum number in an array
+def max_array(arr):
+    max=arr[0]
+    for i in arr:
+        if i> max:
+            max=i
+    print("Maximum number in an array is",max)
+arr=list(map(int,input("Enter Elements:").split()))
+max_array(arr)
