3a926d9ecee249535157a3b0a68648f609487788
Rajesh Allam <rajeshallam9966@gmail.com>
2025-08-12 12:31:39 +0530

    Added min_array

--- Arrays/min_array.py
+++ Arrays/min_array.py
@@ -1,1 +1,9 @@
-
+#finding minimum number in an array
+def min_array(arr):
+    min=arr[0]
+    for i in arr:
+        if i< min:
+            min=i
+    print("Minimum number in an array is",min)
+arr=list(map(int,input("Enter Elements:").split()))
+min_array(arr)
